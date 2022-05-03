c471c491450ec2a60572762729a99872dedef358
Suraj Rawat <sun.rawat37@gmail.com>
2022-05-03 14:21:26 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,6 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require( 'karma-phantomjs-launcher' ),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
